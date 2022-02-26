0e270abc1f33056162150914145031d5e813f472
agusbrav <agus.brav@gmail.com>
2022-02-26 03:57:23 -0300

    Tests for Will contracts

--- test/WillFactory_tests.ts
+++ test/WillFactory_tests.ts
@@ -1,49 +1,60 @@
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { expect } from "chai";
 import { ethers } from "hardhat";
 
-describe("Will Factory Tests", async function () {
+describe("Will Factory Tests", function () {
   const zero_address = "0x0000000000000000000000000000000000000000";
-  const ERROR_MSG0 = "You already have a will contract";
-  const ERROR_MSG1 = "You do not have a deployed Will";
-  const ERROR_MSG2 = "The minimum time is 1 day";
-  const ERROR_MSG3 = "The maximun time is 365 days";
+  let ERROR_MSG: string[];
+  ERROR_MSG = [
+    "You already have a will contract",
+    "You do not have a deployed Will",
+    "The minimum time is 1 day",
+    "The maximun time is 365 days",
+  ];
 
-  const [owner, lawyer, addr2, addr3] = await ethers.getSigners();
+  let account: SignerWithAddress[];
   let contract: any;
+  let WillFactory;
+  let createWill;
 
   before(async function () {
-    const WillFactory = await ethers.getContractFactory("WillFactory");
+    account = await ethers.getSigners();
+    WillFactory = await ethers.getContractFactory("WillFactory");
     contract = await WillFactory.deploy();
     await contract.deployed();
   });
   beforeEach(async function () {});
   it("Should read from the willOwners mapping", async function () {
-    const createWill = await contract.createWillContract(lawyer.address, 60);
+    createWill = await contract.createWillContract(account[1].address, 60);
     await createWill.wait(); /// wait until the transaction is mined
-    expect(await contract.checkWills(owner.address)).to.not.equal(zero_address);
+    expect(await contract.checkWills(account[0].address)).to.not.equal(
+      zero_address
+    );
   });
   it("Should revert if you already have a will contract created", async function () {
     await expect(
-      contract.createWillContract(lawyer.address, 60)
-    ).to.be.revertedWith(ERROR_MSG0);
-    console.log(ERROR_MSG0);
+      contract.createWillContract(account[1].address, 60)
+    ).to.be.revertedWith(ERROR_MSG[0]);
+    console.log(ERROR_MSG[0]);
   });
   it("Should read an empty address without contract", async function () {
-    await expect(contract.checkWills(lawyer.address)).to.be.revertedWith(
-      ERROR_MSG1
+    await expect(contract.checkWills(account[1].address)).to.be.revertedWith(
+      ERROR_MSG[1]
     );
-    console.log(ERROR_MSG1);
+    console.log(ERROR_MSG[1]);
   });
   it("Should revert with wrong time set up < 1", async function () {
     await expect(
-      contract.connect(addr2).createWillContract(lawyer.address, 0)
-    ).to.be.revertedWith(ERROR_MSG2);
-    console.log(ERROR_MSG2);
+      contract.connect(account[3]).createWillContract(account[1].address, 0)
+    ).to.be.revertedWith(ERROR_MSG[2]);
+    console.log(ERROR_MSG[2]);
   });
   it("Should revert with wrong time set up > 365", async function () {
     await expect(
-      contract.connect(addr3).createWillContract(lawyer.address, 32000000)
-    ).to.be.revertedWith(ERROR_MSG3);
-    console.log(ERROR_MSG3);
+      contract
+        .connect(account[4])
+        .createWillContract(account[1].address, 32000000)
+    ).to.be.revertedWith(ERROR_MSG[3]);
+    console.log(ERROR_MSG[3]);
   });
 });
--- test/Will_tests.ts
+++ test/Will_tests.ts
@@ -1,58 +1,91 @@
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { expect } from "chai";
 import { ethers } from "hardhat";
+let account: SignerWithAddress[]; ///From account[5] ahead are payees accounts
+let contract: any;
+let Will;
+let ERROR_MSG: string[];
+const provider = ethers.provider;
 
-describe("setWill Tests from Will", async function () {
-  const [owner, lawyer, addr1, payee1, payee2, payee3] =
-    await ethers.getSigners();
-  const ERROR_MSG0 = "Max payees its 100";
-  const ERROR_MSG1 = "The lawyer cant be also a payee";
-  let addr1B = "0x3c44cdddb6a900fa2b585dd299e03d12fa4293bc";
-  let lawyerRole =
-    "0x6270edb7c868f86fda4adedba75108201087268ea345934db8bad688e1feb91b";
-  const ERROR_MSG2 = `AccessControl: account ${addr1B} is missing role ${lawyerRole}`;
-  let contract: any;
-  const ethValue = ethers.utils.parseEther("1.0");
+const ethValue = ethers.utils.parseEther("1.0");
+const correspondingTokens = ethers.utils.parseEther("0.3");
+const lawyerFee = ethers.utils.parseEther("0.1");
+
+const twentyYearsInSec = 630720000;
+const waitDays = 864000;
+const waitTime = 74649600000;
+let currentTime;
+let currentTimeInSec;
+let unlockTime;
+
+let addrExt = "0x90f79bf6eb2c4f870365e785982e1f101e93b906"; ///External address without Roles in contract (account[3])
+let ownerRole =
+  "0x6270edb7c868f86fda4adedba75108201087268ea345934db8bad688e1feb91b";
+let lawyerRole =
+  "0x26e23ce6b85ff9f368ed1b385a304e4efb5cb11dbc90b57bc31457871b754ae6";
+let payeeRole =
+  "0xdd3cf490277a2ed9b8e9d23db09c21bd229077712bc2c8266158d0d92288625a";
+ERROR_MSG = [
+  "Max payees its 100",
+  "The lawyer cant be also a payee",
+  `AccessControl: account ${addrExt} is missing role ${ownerRole}`,
+  `AccessControl: account ${addrExt} is missing role ${lawyerRole}`,
+  "This will has not been set up",
+  "Already has been executed",
+  `AccessControl: account ${addrExt} is missing role ${payeeRole}`,
+  "Will hasnt been executed yet",
+  "Will hasnt been unlocked yet",
+  "Expiracy date hasnt passed yet",
+];
+describe("Function setWill Tests from Will Contract", function () {
   before(async function () {
-    const Will = await ethers.getContractFactory("Will");
-    contract = await Will.deploy(owner.address, lawyer.address, 864000);
+    account = await ethers.getSigners();
+    Will = await ethers.getContractFactory("Will");
+    contract = await Will.deploy(
+      account[0].address,
+      account[1].address,
+      waitDays
+    );
     await contract.deployed();
   });
   it("Payee adresses should be different from the lawyer address", async function () {
     await expect(
       contract.setWill([
-        payee1.address,
-        payee2.address,
-        payee3.address,
-        lawyer.address,
+        account[5].address,
+        account[6].address,
+        account[7].address,
+        account[1].address, ///Lawyer Address
       ])
-    ).to.be.revertedWith(ERROR_MSG1);
+    ).to.be.revertedWith(ERROR_MSG[1]);
   });
   it("Should update allocations with lawyerfee and the corresponding tokens", async function () {
-    let correspondingTokens = ethers.utils.parseEther("0.3");
-    let lawyerFee = ethers.utils.parseEther("0.1");
-    const twentyYears = 630720000;
+    currentTimeInSec = Math.ceil((Date.now() + 1000) / 1000);
+    await provider.send("evm_setNextBlockTimestamp", [currentTimeInSec]);
     await expect(
-      contract.setWill([payee1.address, payee2.address, payee3.address], {
-        value: ethValue,
-      })
+      contract.setWill(
+        [account[5].address, account[6].address, account[7].address],
+        {
+          value: ethValue,
+        }
+      )
     )
       .to.emit(contract, "WillReport")
       .withArgs(
-        owner.address,
-        lawyer.address,
+        account[0].address,
+        account[1].address,
         0,
         false,
         ethValue,
         correspondingTokens,
         lawyerFee,
-        twentyYears
+        twentyYearsInSec + currentTimeInSec
       );
     console.log(
       "Owner address: ",
-      owner.address,
+      account[0].address,
       "\n",
       "Lawyer address: ",
-      lawyer.address,
+      account[1].address,
       "\n",
       "Total balance: ",
       ethers.utils.formatEther(ethValue),
@@ -67,117 +100,209 @@ describe("setWill Tests from Will", async function () {
       "ETH",
       "\n",
       "Expiracy date: ",
-      new Date()
+      new Date((twentyYearsInSec + currentTimeInSec) * 1000)
     );
   });
   it("Shouldnt be able to setWill With an address thats not the owner", async function () {
     await expect(
-      contract.connect(addr1).setWill([payee1.address, payee2.address])
-    ).to.be.revertedWith(ERROR_MSG2);
+      contract
+        .connect(account[3])
+        .setWill([account[5].address, account[6].address])
+    ).to.be.revertedWith(ERROR_MSG[2]);
     console.log(
       "Owner address:",
-      owner.address,
+      account[0].address,
       "Msg.sender address:",
-      addr1.address
+      account[3].address
     );
   });
   it("Shouldnt be able to load more than 100 payees", async function () {
     for (let i = 0; i < 96; i++) {
-      await contract.setWill([payee1.address]);
+      await contract.setWill([account[5].address]);
     }
     await expect(
-      contract.setWill([payee1.address, payee2.address, payee3.address])
-    ).to.be.revertedWith(ERROR_MSG0);
+      contract.setWill([
+        account[5].address,
+        account[6].address,
+        account[7].address,
+      ])
+    ).to.be.revertedWith(ERROR_MSG[0]);
     console.log();
   });
 });
-describe("executeWill from Will", async function () {
-  const [owner, lawyer, addr1, payee1, payee2, payee3] =
-    await ethers.getSigners();
-  const ERROR_MSG0 = "Max payees its 100";
-  const ERROR_MSG1 = "The lawyer cant be also a payee";
-  let addr1B = "0x3c44cdddb6a900fa2b585dd299e03d12fa4293bc";
-  let lawyerRole =
-    "0x6270edb7c868f86fda4adedba75108201087268ea345934db8bad688e1feb91b";
-  const ERROR_MSG2 = `AccessControl: account ${addr1B} is missing role ${lawyerRole}`;
-  let contract: any;
-  const ethValue = ethers.utils.parseEther("1.0");
+
+describe("Function executeWill from Will Contract", function () {
   before(async function () {
-    const Will = await ethers.getContractFactory("Will");
-    contract = await Will.deploy(owner.address, lawyer.address, 864000);
+    account = await ethers.getSigners();
+    Will = await ethers.getContractFactory("Will");
+    contract = await Will.deploy(
+      account[0].address,
+      account[1].address,
+      waitDays
+    );
     await contract.deployed();
   });
-  it("Payee adresses should be different from the lawyer address", async function () {
-    await expect(
-      contract.setWill([
-        payee1.address,
-        payee2.address,
-        payee3.address,
-        lawyer.address,
-      ])
-    ).to.be.revertedWith(ERROR_MSG1);
+  it("Only the lawyer account(account[1]) should call this function", async function () {
+    await expect(contract.connect(account[3]).executeWill()).to.be.revertedWith(
+      ERROR_MSG[3]
+    );
+    console.log(
+      "Lawyer address:",
+      account[1].address,
+      "Msg.sender address:",
+      account[3].address
+    );
   });
-  it("Should update allocations with lawyerfee and the corresponding tokens", async function () {
-    let correspondingTokens = ethers.utils.parseEther("0.3");
-    let lawyerFee = ethers.utils.parseEther("0.1");
-    const twentyYears = 630720000;
-    await expect(
-      contract.setWill([payee1.address, payee2.address, payee3.address], {
+  it("Shouldnt be able to executeWill if it hasnt been set up", async function () {
+    await expect(contract.connect(account[1]).executeWill()).to.be.revertedWith(
+      ERROR_MSG[4]
+    );
+  });
+  it("Should be able to executeWill from lawyer if its set up with payees and balance", async function () {
+    await contract.setWill(
+      [account[5].address, account[6].address, account[7].address],
+      {
         value: ethValue,
-      })
-    )
-      .to.emit(contract, "WillReport")
+      }
+    );
+    currentTime = Date.now() + 1000;
+    await provider.send("evm_setNextBlockTimestamp", [currentTime]);
+    await expect(contract.connect(account[1]).executeWill())
+      .to.emit(contract, "WillExecuted")
       .withArgs(
-        owner.address,
-        lawyer.address,
-        0,
-        false,
+        true,
+        waitTime,
+        account[1].address,
+        waitTime + currentTime,
         ethValue,
-        correspondingTokens,
-        lawyerFee,
-        twentyYears
+        3
       );
     console.log(
-      "Owner address: ",
-      owner.address,
+      "Will is executed: ",
+      true,
       "\n",
       "Lawyer address: ",
-      lawyer.address,
+      account[1].address,
       "\n",
       "Total balance: ",
       ethers.utils.formatEther(ethValue),
       "ETH",
       "\n",
-      "Corresponding per payee: ",
-      ethers.utils.formatEther(correspondingTokens),
-      "ETH",
-      "\n",
-      "Lawyer fee: ",
-      ethers.utils.formatEther(lawyerFee),
-      "ETH",
-      "\n",
-      "Expiracy date: ",
-      new Date()
+      "Unlock date: ",
+      new Date(waitTime + currentTime)
     );
   });
-  it("Shouldnt be able to setWill With an address thats not the owner", async function () {
+  it("Shouldnt be able to executeWill if it was already executed", async function () {
+    await expect(contract.connect(account[1]).executeWill()).to.be.revertedWith(
+      ERROR_MSG[5]
+    );
+  });
+});
+
+describe("Function withdrawShares from Will Contract (after executeWill) part 1", function () {
+  before(async function () {
+    account = await ethers.getSigners();
+    Will = await ethers.getContractFactory("Will");
+    contract = await Will.deploy(
+      account[0].address,
+      account[1].address,
+      waitDays
+    );
+    await contract.deployed();
+    await contract.setWill(
+      [account[5].address, account[6].address, account[7].address],
+      {
+        value: ethValue,
+      }
+    );
+  });
+  it("Only the payees account(from account[5]) should call this function", async function () {
     await expect(
-      contract.connect(addr1).setWill([payee1.address, payee2.address])
-    ).to.be.revertedWith(ERROR_MSG2);
+      contract.connect(account[3]).withdrawShares()
+    ).to.be.revertedWith(ERROR_MSG[6]);
     console.log(
-      "Owner address:",
-      owner.address,
-      "Msg.sender address:",
-      addr1.address
+      "Payee addresses: ",
+      account[5].address,
+      "\n",
+      "Msg.sender address (Owner): ",
+      account[1].address
     );
   });
-  it("Shouldnt be able to load more than 100 payees", async function () {
-    for (let i = 0; i < 96; i++) {
-      await contract.setWill([payee1.address]);
-    }
+  it("Shouldnt be able to withdrawShares if it hasnt been executed", async function () {
     await expect(
-      contract.setWill([payee1.address, payee2.address, payee3.address])
-    ).to.be.revertedWith(ERROR_MSG0);
-    console.log();
+      contract.connect(account[5]).withdrawShares()
+    ).to.be.revertedWith(ERROR_MSG[7]);
+  });
+  it("Shouldnt be able to withdrawShares if the unlock time hasnt reached", async function () {
+    await contract.connect(account[1]).executeWill();
+    await expect(
+      contract.connect(account[5]).withdrawShares()
+    ).to.be.revertedWith(ERROR_MSG[8]);
+  });
+  it("Executing withdraShares after unlockTime and executeWill", async function () {
+    currentTime = Date.now() + 1000;
+    unlockTime = currentTime + waitTime;
+    await provider.send("evm_setNextBlockTimestamp", [unlockTime]);
+    await expect(contract.connect(account[5]).withdrawShares())
+      .to.emit(contract, "SharesWithdrawn")
+      .withArgs(ethValue, lawyerFee, correspondingTokens, account[5].address);
+  });
+});
+
+describe("Function withdrawShares from Will Contract (after executeWill) Part 2 (1st part selfdestructed after withdrawShares)", function () {
+  before(async function () {
+    account = await ethers.getSigners();
+    Will = await ethers.getContractFactory("Will");
+    contract = await Will.deploy(
+      account[0].address,
+      account[1].address,
+      waitDays
+    );
+    await contract.deployed();
+    await contract.setWill(
+      [account[5].address, account[6].address, account[7].address],
+      {
+        value: ethValue,
+      }
+    );
+    await contract.connect(account[1]).executeWill();
+  });
+  it("Calling reclaimOwnerBalance before expire date", async function () {
+    await expect(
+      contract.connect(account[0]).reclaimOwnerBalance()
+    ).to.be.revertedWith(ERROR_MSG[9]);
+  });
+  it("Calling resetWill and changing lawyer", async function () {
+    await expect(contract.resetWill(account[4].address))
+      .to.emit(contract, "ChangedLawyer")
+      .withArgs(account[1].address, account[4].address);
+    console.log(
+      "Old lawyer: ",
+      account[1].address,
+      "\n",
+      "New lawyer: ",
+      account[4].address
+    );
+  });
+  it("Calling reclaimOwnerBalance after expire date with new lawyer", async function () {
+    let afterExpireTime = Date.now() + twentyYearsInSec;
+    await provider.send("evm_increaseTime", [afterExpireTime]);
+    let ownerBalanceBefore = ethers.utils.formatEther(
+      await provider.getBalance(account[0].address)
+    );
+    console.log(
+      "Balance before: ",
+      ethers.utils.formatEther(await provider.getBalance(account[0].address))
+    );
+    let txReclaim = await contract.reclaimOwnerBalance();
+    await txReclaim.wait();
+    let ownerBalanceAfter = ethers.utils.formatEther(
+      await provider.getBalance(account[0].address)
+    );
+    console.log(
+      "Balance after: ",
+      ethers.utils.formatEther(await provider.getBalance(account[0].address))
+    );
+    expect(ownerBalanceBefore).to.not.equal(ownerBalanceAfter);
   });
 });
