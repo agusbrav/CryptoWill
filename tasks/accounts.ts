d37f940d58ea4b6fec92efe4b9f5e46affc0ddfd
agusbrav <agus.brav@gmail.com>
2022-05-19 12:34:04 -0300

    Added function to get randomSigners for tests

--- tasks/accounts.ts
+++ tasks/accounts.ts
@@ -1,4 +1,5 @@
 import { Signer } from "@ethersproject/abstract-signer";
+import { ethers } from "ethers";
 import { task } from "hardhat/config";
 
 task("accounts", "Prints the list of accounts", async (_taskArgs, hre) => {
@@ -8,3 +9,11 @@ task("accounts", "Prints the list of accounts", async (_taskArgs, hre) => {
     console.log(await account.getAddress());
   }
 });
+
+export const randomSigners = (amount: number): Signer[] => {
+  const signers: Signer[] = [];
+  for (let i = 0; i < amount; i++) {
+    signers.push(ethers.Wallet.createRandom());
+  }
+  return signers;
+};
