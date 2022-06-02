72f4d67b34f109cd5742e2694a1462bee5c2e1aa
agusbrav <agus.brav@gmail.com>
2022-06-01 21:41:42 -0300

    Create dummy contract to import ERC721 and ERC20 presets and generate artifacts for tests

---
+++ contracts/test/Test.sol
@@ -0,0 +1,14 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.14;
+import "@openzeppelin/contracts/token/ERC20/presets/ERC20PresetFixedSupply.sol";
+import "@openzeppelin/contracts/token/ERC721/presets/ERC721PresetMinterPauserAutoId.sol";
+
+/**
+ * @dev Dummy contract to import contract artifacts for testing.
+ */
+contract Test {
+    function foo() public pure returns (bool) {
+        return true;
+    }
+}
